d98fcdf0ecbd911f749b051ce2b61699925ab8d6
Arya <ngurahpratama2002@gmail.com>
2024-10-04 13:29:30 +0700

    Adding LMS Fiture assets & section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
+import { fiturCards, CardProps } from "./assets/landingData";
+
 export default function App() {
+  const Card: React.FC<CardProps> = ({ icon, title }) => (
+    <div className="bg-white flex-col justify-center w-72 h-32 p-4 rounded-lg shadow-md flex items-center">
+      <img src={icon} alt={title} className="w-12 h-12 " />
+      <div>
+        <h2 className="text-base pt-2 text-center font-bold">{title}</h2>
+      </div>
+    </div>
+  );
+
   return (
     <>
+      {/* Hero */}
       <section
         className="h-screen bg-cover bg-no-repeat bg-center max-w-screen flex items-center justify-center"
         style={{
           backgroundImage: "url('/landing/hero-bg.png')",
         }}
       >
-        <div className="flex w-5/6  h-full">
-          <div className="w-11/12  h-full flex flex-col justify-center text-left  ">
+        <div className="flex w-5/6 h-full">
+          <div className="w-11/12 h-full flex flex-col justify-center text-left">
             <h1 className="text-3xl font-light text-gray-600 pb-5">
               Pelatihan
             </h1>
@@ -23,7 +35,7 @@ export default function App() {
               className="w-56 bg-white bg-opacity-20 rounded my-10"
               alt=""
             />
-            <div className="flex items-center ">
+            <div className="flex items-center">
               <button className="w-56 h-14 bg-[#FAB317] text-white font-bold text-xl rounded-lg">
                 Klik Disini
               </button>
@@ -32,8 +44,23 @@ export default function App() {
               </h1>
             </div>
           </div>
-          <div className=" flex items-center">
-            <img src="/landing/hero-right.png" className="" alt="" />
+          <div className="flex items-center">
+            <img src="/landing/hero-right.png" alt="" />
+          </div>
+        </div>
+      </section>
+      {/* Fitur LMS */}
+      <section className="min-h-screen">
+        <div className="w-11/12 mx-auto py-14 bg-slate-300">
+          <h1 className="text-5xl text-[#106FA4] text-center font-bold pb-14">
+            Fitur <span className="text-[#FAB317]">LMS M-Knows</span>
+          </h1>
+          <div className="container mx-auto">
+            <div className="flex flex-wrap mx-auto justify-center gap-9">
+              {fiturCards.map((card, index) => (
+                <Card key={index} icon={card.icon} title={card.title} />
+              ))}
+            </div>
           </div>
         </div>
       </section>
---
+++ src/assets/landingData.ts
@@ -0,0 +1,87 @@
+export interface CardProps {
+  icon: string;
+  title: string;
+}
+
+export const fiturCards: CardProps[] = [
+  {
+    icon: "/landing/fitur/bootcamp.png",
+    title: "Bootcamp",
+  },
+  {
+    icon: "/landing/fitur/rencana-studi.png",
+    title: "Rencana Studi",
+  },
+  {
+    icon: "/landing/fitur/bootcamp.png",
+    title: "Studi-Ku",
+  },
+  {
+    icon: "/landing/fitur/penugasan.png",
+    title: "Penugasan",
+  },
+  {
+    icon: "/landing/fitur/ruang-diskusi.png",
+    title: "Ruang Diskusi",
+  },
+  {
+    icon: "/landing/fitur/roleplay.png",
+    title: "Roleplay & Asesmen",
+  },
+  {
+    icon: "/landing/fitur/roleplay.png",
+    title: "Drill, Simulasi & Uji Kompetensi",
+  },
+  {
+    icon: "/landing/fitur/nilai.png",
+    title: "Nilai & Sertifikat",
+  },
+  {
+    icon: "/landing/fitur/analitik.png",
+    title: "Analitik",
+  },
+  {
+    icon: "/landing/fitur/papan-skor.png",
+    title: "Gamifikasi (Skor)",
+  },
+  {
+    icon: "/landing/fitur/perencanaan-karir.png",
+    title: "Perencanaan Karir",
+  },
+  {
+    icon: "/landing/fitur/rencana-studi.png",
+    title: "Konsultasi & Bimbingan",
+  },
+  {
+    icon: "/landing/fitur/kredit-kompetensi.png",
+    title: "Kredit Kompetensi",
+  },
+  {
+    icon: "/landing/fitur/roleplay.png",
+    title: "Kalender Saya",
+  },
+  {
+    icon: "/landing/fitur/acara.png",
+    title: "Acara",
+  },
+  {
+    icon: "/landing/fitur/panduan.png",
+    title: "Panduan",
+  },
+  {
+    icon: "/landing/fitur/rencana-studi.png",
+    title: "Layanan",
+  },
+  {
+    icon: "/landing/fitur/sekilas-ilmu.png",
+    title: "Sekilas Ilmu",
+  },
+  {
+    icon: "/landing/fitur/sekilas-ilmu.png",
+    title: "Proyek Improvement",
+  },
+  {
+    icon: "/landing/fitur/sekilas-ilmu.png",
+    title: "MT & MDP",
+  },
+];
