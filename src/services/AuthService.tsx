147da9535484a75378360b5060e1aef68462545e
Arya <ngurahpratama2002@gmail.com>
2024-10-09 19:02:25 +0700

    Manually endpoint on auth.ts

--- src/services/AuthService.tsx
+++ src/services/AuthService.tsx
@@ -2,13 +2,15 @@ import axios from "axios";
 import { LoginResponse } from "../types/auth";
 import { API_URL } from "../config/api";
 
+// https://api.m-knows.com/api/v1/auth/login-username
+
 export const login = async (
   username: string,
   password: string
 ): Promise<LoginResponse> => {
   try {
     const response = await axios.post<LoginResponse>(
-      `${API_URL}/login-username`,
+      `https://api.m-knows.com/api/v1/auth/login-username`,
       { username, password }
     );
     return response.data;
