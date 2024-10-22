21cfefca4c387b8e61926bb134b64cdd2448a90d
Arya <ngurahpratama2002@gmail.com>
2024-10-22 11:12:27 +0700

    Adding responsive to pelatihanku page

--- src/pages/pelatihanku/components/PelatihankuCard.tsx
+++ src/pages/pelatihanku/components/PelatihankuCard.tsx
@@ -7,10 +7,12 @@ export const TrainingCard: React.FC<{ training: Training }> = ({
   return (
     <Link to={`/pelatihanku/${training.id}`} className="block">
       <div className="border rounded-xl p-4 mb-4 shadow-md hover:shadow-lg transition-shadow">
-        <h3 className="font-semibold text-2xl">{training.title}</h3>
-        <p className="text-gray-500 py-2">{training.instructor}</p>
-        <div className="flex space-x-5 mt-2">
-          <div className="bg-blue-100 px-2 w-1/3 h-14 rounded-lg py-1 flex items-center justify-center text-blue-700">
+        <h3 className="font-semibold md:text-2xl text-lg">{training.title}</h3>
+        <p className="text-gray-500 py-2 md:text-base text-sm">
+          {training.instructor}
+        </p>
+        <div className="flex md:flex-row flex-col items-center md:space-x-5 md:space-y-0 space-y-5 mt-2">
+          <div className="bg-blue-100 px-2 md:w-1/3 w-2/3 h-14 rounded-lg py-1 flex items-center justify-center text-blue-700">
             <img
               src="/pelatihanku/sks.png"
               alt="Icon SKS"
@@ -18,7 +20,7 @@ export const TrainingCard: React.FC<{ training: Training }> = ({
             />
             {training.credits} SKS
           </div>
-          <div className="bg-green-100 px-2 w-1/3 h-14 rounded-lg py-1 flex items-center justify-center text-green-700">
+          <div className="bg-green-100 px-2 md:w-1/3 w-2/3 h-14 rounded-lg py-1 flex items-center justify-center text-green-700">
             <img
               src="/pelatihanku/video.png"
               alt="Icon Video"
@@ -26,7 +28,7 @@ export const TrainingCard: React.FC<{ training: Training }> = ({
             />
             {training.videos} Video
           </div>
-          <div className="bg-purple-100 px-2 w-1/3 h-14 rounded-lg py-1 flex items-center justify-center text-purple-700">
+          <div className="bg-purple-100 px-2 md:w-1/3 w-2/3 h-14 rounded-lg py-1 flex items-center justify-center text-purple-700">
             <img
               src="/pelatihanku/member.png"
               alt="Icon Peserta"
