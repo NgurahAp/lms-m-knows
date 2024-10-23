efba4e7d1aa8dad3fbc92ab8204fb3674d966b89
Arya <ngurahpratama2002@gmail.com>
2024-10-23 17:13:47 +0700

    Done integrating my study page to api

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "https://api.m-knows.com/api/v1";
+export const API_BASE_URL = "https://api.m-knows.com";
--- src/pages/pelatihanku/components/MyStudyCard.tsx
+++ src/pages/pelatihanku/components/MyStudyCard.tsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
-import { Training } from "./pelatihanData";
+import { MyStudyData } from "../../../types/pelatihanku";
 
-export const TrainingCard: React.FC<{ training: Training }> = ({
+export const MyStudyCard: React.FC<{ training: MyStudyData }> = ({
   training,
 }) => {
   return (
     <Link to={`/pelatihanku/${training.id}`} className="block">
       <div className="border rounded-xl p-4 mb-4 shadow-md hover:shadow-lg transition-shadow">
-        <h3 className="font-semibold md:text-2xl text-lg">{training.title}</h3>
+        <h3 className="font-semibold md:text-2xl text-lg">{training.name}</h3>
         <p className="text-gray-500 py-2 md:text-base text-sm">
-          {training.instructor}
+          {training.teacher_name}
         </p>
         <div className="flex md:flex-row flex-col items-center md:space-x-5 md:space-y-0 space-y-5 mt-2">
           <div className="bg-blue-100 px-2 md:w-1/3 w-2/3 h-14 rounded-lg py-1 flex items-center justify-center text-blue-700">
@@ -18,7 +18,7 @@ export const TrainingCard: React.FC<{ training: Training }> = ({
               alt="Icon SKS"
               className="w-6 h-6 mr-2"
             />
-            {training.credits} SKS
+            {training.credit} SKS
           </div>
           <div className="bg-green-100 px-2 md:w-1/3 w-2/3 h-14 rounded-lg py-1 flex items-center justify-center text-green-700">
             <img
@@ -26,7 +26,7 @@ export const TrainingCard: React.FC<{ training: Training }> = ({
               alt="Icon Video"
               className="w-6 h-6 mr-2"
             />
-            {training.videos} Video
+            {training.session_count} Video
           </div>
           <div className="bg-purple-100 px-2 md:w-1/3 w-2/3 h-14 rounded-lg py-1 flex items-center justify-center text-purple-700">
             <img
@@ -34,7 +34,7 @@ export const TrainingCard: React.FC<{ training: Training }> = ({
               alt="Icon Peserta"
               className="w-6 h-6 mr-2"
             />
-            {training.participants} Peserta
+            {training.student_count} Peserta
           </div>
         </div>
         <div className="mt-4">
@@ -42,10 +42,12 @@ export const TrainingCard: React.FC<{ training: Training }> = ({
           <div className="w-full bg-gray-200 rounded-full h-2.5">
             <div
               className="bg-blue-500 h-2.5 rounded-full"
-              style={{ width: `${training.completion}%` }}
+              style={{ width: `${training.progress_percentage}%` }}
             ></div>
           </div>
-          <p className="text-sm text-gray-600 mt-2">{training.completion}%</p>
+          <p className="text-sm text-gray-600 mt-2">
+            {training.progress_percentage}%
+          </p>
         </div>
       </div>
     </Link>
