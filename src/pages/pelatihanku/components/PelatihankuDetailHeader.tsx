f9a4560cade3be0da82a9868d428cc8e16b104c4
Arya <ngurahpratama2002@gmail.com>
2024-10-29 14:47:54 +0700

    Done bug on detail session

---
+++ src/pages/pelatihanku/components/PelatihankuDetailHeader.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import { FaChevronRight } from "react-icons/fa";
+
+export const PelatihankuDetailHeader = () => {
+  return (
+    <div className="bg-white w-full h-14 flex items-center pl-5 rounded-xl">
+      <Link to="/dashboard" className="flex items-center">
+        <img
+          src="/pelatihanku/home.png"
+          className="md:w-6 w-5 -mt-1 "
+          alt="Home"
+        />
+        <span className="md:pl-5 pl-3 text-blue-500 md:text-base text-sm font-semibold">
+          Beranda
+        </span>
+      </Link>
+      <FaChevronRight className="text-gray-300 mx-4" />
+      <span className="text-[#9CA3AF] md:text-base text-sm font-semibold">
+        Pelatihan-ku
+      </span>
+    </div>
+  );
+};
