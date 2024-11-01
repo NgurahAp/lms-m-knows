3ca8f4179c9291f699516f308b1219bd8343e416
Arya <ngurahpratama2002@gmail.com>
2024-11-01 10:58:58 +0700

    Done fetching all the data on detail module page

--- src/pages/pelatihanku/modul/DetailModul.tsx
+++ src/pages/pelatihanku/modul/DetailModul.tsx
@@ -3,6 +3,7 @@ import { FaCheck, FaChevronRight } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
 import { IoDocumentText } from "react-icons/io5";
 import { useDetailModuleData } from "../../../services/modul/ModulService";
+import { RiFileEditLine } from "react-icons/ri";
 
 export const DetailModule = () => {
   const { subjectId, sessionId, moduleId } = useParams<{
@@ -55,18 +56,24 @@ export const DetailModule = () => {
           </span>
         </Link>
         <FaChevronRight className="text-gray-300 mx-4" />
-        <Link to="">
+        <Link to={`/pelatihanku/${subjectId}`}>
           <span className="text-blue-500 md:text-base text-sm font-semibold">
-            Jduul
+            {data?.detail.subject_name}
+          </span>
+        </Link>
+        <FaChevronRight className="text-gray-300 mx-4" />
+        <Link to={`/modul/${subjectId}/${sessionId}`}>
+          <span className="text-blue-500 md:text-base text-sm font-semibold">
+            Pertemuan {data?.detail.session_no}
           </span>
         </Link>
         <FaChevronRight className="text-gray-300 mx-4" />
         <span className="text-gray-400 md:text-base text-sm font-semibold">
-          Pertemuan
+          {data?.module.title}
         </span>
       </div>
       <div className="bg-white flex flex-col items-center mt-5 px-8 py-20 justify-center rounded-lg">
-        <h1 className="font-bold text-4xl mb-8">Modul Pertama</h1>
+        <h1 className="font-bold text-4xl mb-8">{data?.module.title}</h1>
         <div className="flex flex-col md:flex-row justify-center items-start gap-6  bg-white w-full">
           {/* Left Section */}
           <div className="flex flex-col items-center h-[73vh] bg-white rounded-lg shadow-md p-6 w-full md:w-1/2">
@@ -80,17 +87,27 @@ export const DetailModule = () => {
             </div>
             <div className="w-full flex flex-col pt-14">
               <hr className="border-t-[1px] border-gray-300 w-full" />
-              <p className="text-gray-500 pt-5">Tidak ada deskripsi</p>
+              <p className="text-gray-500 pt-5">
+                {data?.module.description && data.module.description !== "-"
+                  ? data.module.description
+                  : "Tidak ada deskripsi"}
+              </p>
             </div>
-            <div className="mt-8 flex items-center justify-between w-full text-blue-500">
+            <Link
+              to={`/modul/${subjectId}/${sessionId}`}
+              className="mt-8 flex items-center justify-between w-full text-blue-500"
+            >
               <p className="flex items-center gap-2 underline">
                 <FaArrowLeft />
                 Kembali
               </p>
-              <button className=" px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+              <Link
+                to={`/quis/${subjectId}/${sessionId}`}
+                className=" px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+              >
                 Lanjutkan Ke Kuis
-              </button>
-            </div>
+              </Link>
+            </Link>
           </div>
 
           <div className="flex flex-col h-[73vh] justify-between bg-white rounded-lg shadow-md p-8 w-full md:w-1/2 ">
@@ -102,10 +119,16 @@ export const DetailModule = () => {
               </div>
               <div className="mb-4">
                 <h2 className="text-gray-700 font-semibold ">Dokumen</h2>
-                <button className="text-gray-500 flex items-center w-full border-[1px] rounded-lg p-4 my-5 gap-3">
-                  <IoDocumentText className="text-3xl text-red-500" />
-                  BSI RBC Peran dan kompetensi anti fraud-DP
-                </button>
+                {data?.module.documents.map((doc, index) => (
+                  <button
+                    key={index}
+                    onClick={() => window.open(doc.document_file, "_blank")}
+                    className="text-gray-500 flex items-center w-full border-[1px] rounded-lg p-4 my-5 gap-3"
+                  >
+                    <IoDocumentText className="text-3xl text-red-500" />
+                    <span>{doc.title || "Document"}</span>
+                  </button>
+                ))}
                 <hr className="border-t-[1px] border-gray-300 w-full" />
               </div>
               <div className="mb-4">
@@ -120,12 +143,21 @@ export const DetailModule = () => {
               </div>
             </div>
             <div className="flex justify-center">
-              <button className="mt-4 px-20 py-4 flex rounded-lg items-center bg-green-500 text-xl gap-3 text-white  hover:bg-green-600">
-                <div className="bg-white rounded-full">
-                  <FaCheck className="text-green-600 p-1 text-xl" />
-                </div>
-                Modul Selesai
-              </button>
+              <div className="flex justify-center">
+                {data?.module.status === "finished" ? (
+                  <button className="mt-4 px-20 py-4 flex rounded-lg items-center bg-green-500 text-xl gap-3 text-white hover:bg-green-600">
+                    <div className="bg-white rounded-full">
+                      <FaCheck className="text-green-600 p-1 text-xl" />
+                    </div>
+                    Modul Selesai
+                  </button>
+                ) : (
+                  <button className="mt-4 px-20 py-4 flex rounded-lg items-center bg-blue-500 text-xl gap-2 text-white hover:bg-blue-600">
+                    <RiFileEditLine className="text-white text-2xl" />
+                    Selesaikan Modul
+                  </button>
+                )}
+              </div>
             </div>
           </div>
         </div>
