ff29ccb359930619c12977b1c7093dbd9a376898
Arya <ngurahpratama2002@gmail.com>
2024-11-07 11:23:58 +0700

    Done slicing pengumpulan tugas

--- src/pages/pelatihanku/assignments/DetailAssignment.tsx
+++ src/pages/pelatihanku/assignments/DetailAssignment.tsx
@@ -2,7 +2,7 @@ import { Link, useParams } from "react-router-dom";
 import { FaChevronRight } from "react-icons/fa";
 import { useDetailAssignmentData } from "../../../services/pelatihanku/AssignmentService";
 import { useState } from "react";
-import { FaUpload } from "react-icons/fa6";
+import { FaUpload, FaFileWord, FaFileImage } from "react-icons/fa6";
 import { CiSearch } from "react-icons/ci";
 import { PageInfo } from "../../../components/reusable/PageInfo";
 
@@ -15,6 +15,23 @@ export const DetailAssignment = () => {
 
   const [description, setDescription] = useState("");
   const [file, setFile] = useState<File | null>(null);
+
+  const getFileIcon = (fileName: string) => {
+    const extension = fileName.split(".").pop()?.toLowerCase();
+    switch (extension) {
+      case "pdf":
+        return <img src="/penugasan/pdf.png" className="w-1/3" alt="" />;
+      case "doc":
+      case "docx":
+        return <FaFileWord className="text-blue-500 text-4xl" />;
+      case "jpg":
+      case "jpeg":
+      case "png":
+        return <FaFileImage className="text-green-500 text-4xl" />;
+      default:
+        return <FaUpload className="text-gray-500 text-4xl" />;
+    }
+  };
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
@@ -32,6 +49,7 @@ export const DetailAssignment = () => {
     sessionId,
     assignmentId
   );
+
   if (isLoading) {
     return (
       <div className="min-h-[85vh] w-screen flex items-center justify-center">
@@ -91,10 +109,10 @@ export const DetailAssignment = () => {
         detail={`Modul ${data?.data.detail.session_no}`}
       />
       {/* Quiz Content */}
-      <div className="bg-white  mt-5 w-full p-8 h-full rounded-lg">
+      <div className="bg-white mt-5 w-full p-8 h-full rounded-lg">
         <h1 className="font-bold">{data?.data.assignment.title}</h1>
         <p className="pt-5 whitespace-pre-line">{data?.data.assignment.desc}</p>
-        <div className="border-b-[1px]  border-gray-400 mt-10" />
+        <div className="border-b-[1px] border-gray-400 mt-10" />
         {/* Status */}
         <div>
           <h1 className="text-2xl pt-10 font-bold">Penyerahan Berkas</h1>
@@ -123,35 +141,53 @@ export const DetailAssignment = () => {
               >
                 Unggah Berkas File
               </label>
-              <div className="mt-2 border-2 border-dashed border-gray-300 bg-gray-50 py-20 rounded-md">
+              <div className="mt-2 border-2 border-dashed border-gray-300 bg-gray-50 py-3 rounded-md">
                 <input
                   type="file"
                   id="file"
                   className="hidden"
                   onChange={handleFileChange}
-                  accept=".jpg,.png,.pdf, .doc, .docx"
+                  accept=".jpg,.png,.pdf,.doc,.docx"
                 />
 
-                {file && (
-                  <div className="mt-2 text-sm text-gray-700">
-                    File Terpilih: {file.name}
-                  </div>
-                )}
                 <label
                   htmlFor="file"
-                  className="block text-center text-gray-500 cursor-pointer"
+                  className="block text-center h-56 text-gray-500 cursor-pointer"
                 >
-                  <FaUpload className="flex justify-center items-center w-full text-4xl pb-3" />
-                  Klik untuk upload atau drag and drop
-                  <p className="mt-1 text-xs text-gray-500">
-                    Max. File Size: 2MB
-                  </p>
-                  <div className="flex justify-center pt-5">
-                    <button className="flex gap-2 py-2 px-3 text-sm bg-blue-500 text-white rounded-lg items-center justify-center">
-                      <CiSearch />
-                      Cari File
-                    </button>
-                  </div>
+                  {file ? (
+                    <div className="w-1/5">
+                      <div className="flex justify-between  mx-3 p-3 border border-gray-200 rounded-t-md bg-white">
+                        <p className="font-medium text-gray-700 text-left pr-5">
+                          {file.name}
+                        </p>
+
+                        <button
+                          type="button"
+                          onClick={() => setFile(null)}
+                          className=" text-3xl"
+                        >
+                          ✕
+                        </button>
+                      </div>
+                      <div className="mx-3 h-36 bg-gray-300">
+                        <div className="flex items-center justify-center h-full">{getFileIcon(file.name)}</div>
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="flex flex-col justify-center items-center h-full">
+                      <FaUpload className="flex justify-center items-center w-full text-4xl pb-3" />
+                      Klik untuk upload atau drag and drop
+                      <p className="mt-1 text-xs text-gray-500">
+                        Max. File Size: 2MB
+                      </p>
+                      <div className="flex justify-center pt-5">
+                        <div className="flex gap-2 py-2 px-3 text-sm bg-blue-500 text-white rounded-lg items-center justify-center">
+                          <CiSearch />
+                          Cari File
+                        </div>
+                      </div>
+                    </div>
+                  )}
                 </label>
               </div>
             </div>
@@ -177,3 +213,5 @@ export const DetailAssignment = () => {
     </div>
   );
 };
+
+export default DetailAssignment;
