5a519cbe17ebed87cb5827c36337b848356ec5f0
Arya <ngurahpratama2002@gmail.com>
2024-11-11 11:57:55 +0700

    Done Slicing submit reflection

--- src/pages/pelatihanku/reflection/SubmitReflection.tsx
+++ src/pages/pelatihanku/reflection/SubmitReflection.tsx
@@ -1,6 +1,7 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useReflectionData } from "../../../hooks/pelatihanku/useReflection";
 import { Breadcrumb } from "../../../components/reusable/BreadCrumbs";
+import { FaArrowLeft } from "react-icons/fa6";
 
 export const SubmitReflection = () => {
   const { subjectId, sessionId } = useParams<{
@@ -57,7 +58,41 @@ export const SubmitReflection = () => {
         <p className="text-lg">Pertemuan {data?.data.session_no}</p>
       </div>
       {/* Content */}
-      <div className="p-8 my-8 bg-white"></div>
+      <div className="p-8 my-8 bg-white">
+        <div className="border p-5 rounded-lg">
+          <div className="flex items-center">
+            <img
+              src="https://via.placeholder.com/40"
+              alt="Avatar"
+              className="w-10 h-10 rounded-full mr-3"
+            />
+            <div>
+              <p className="font-bold text-gray-800">Andri Hendrawan</p>
+              <p className="text-sm text-gray-600">Pengajar</p>
+            </div>
+          </div>
+          <h3 className="text-lg font-semibold pt-5 pb-8">
+            Apa pembelajaran dan kesimpulan kamu dari pertemuan ini?
+          </h3>
+          <textarea
+            placeholder="Masukkan jawaban anda..."
+            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            rows={8}
+          ></textarea>
+          <div className="flex justify-end mt-4">
+            <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+              Selesai
+            </button>
+          </div>
+        </div>
+        <Link
+          to={`/reflection/${subjectId}/${sessionId}`}
+          className="flex items-center gap-2 p-5 underline justify-start text-blue-500"
+        >
+          <FaArrowLeft />
+          Kembali
+        </Link>
+      </div>
     </div>
   );
 };
