c5b5cd5b0e6a11e8a2d114ec864c5fe6007b79ca
Arya <ngurahpratama2002@gmail.com>
2024-11-21 11:33:08 +0700

    Done Functional Detail Score

--- src/pages/score/DetailScore.tsx
+++ src/pages/score/DetailScore.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useNilaiData } from "../../services/PelatihanKetService";
 import LoadingSpinner from "../../components/reusable/LoadingSpinner";
 import { Breadcrumb } from "../../components/reusable/BreadCrumbs";
+import { Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
 
 export const DetailScore: React.FC = () => {
   const {
@@ -53,35 +55,38 @@ export const DetailScore: React.FC = () => {
         <div className="overflow-x-auto border rounded-xl shadow-md">
           <table className="min-w-full bg-white border border-gray-300 text-sm">
             <thead>
-              <tr className="h-14 px-4 py-4 border-b-2 border-gray-200 text-center bg-blue-500 text-white">
-                <th className="py-2 px-4 border">Judul</th>
-                <th className="py-2 px-4 border">Modul</th>
-                <th className="py-2 px-4 border">Quiz</th>
-                <th className="py-2 px-4 border">Penugasan</th>
-                <th className="py-2 px-4 border">Rata-rata</th>
-                <th className="py-2 px-4 border">Nilai</th>
+              <tr className="h-14 px-4 py-4 border-b-2 border-gray-200 text-lg  text-center bg-blue-500 text-white">
+                <th className="py-2 px-4 border font-normal">Judul</th>
+                <th className="py-2 px-6 border font-normal">Modul</th>
+                <th className="py-2 px-6 border font-normal">Quiz</th>
+                <th className="py-2 px-6 border font-normal">Penugasan</th>
+                <th className="py-2 px-6 border font-normal">Rata-rata</th>
+                <th className="py-2 px-6 border font-normal">Nilai</th>
               </tr>
             </thead>
             <tbody>
               {sessions.map((session) => (
-                <tr key={session.id} className="text-center">
-                  <td className="py-2 px-4 border-b-2 border-gray-200 text-left">
+                <tr key={session.id} className="text-center font-medium">
+                  <td className="p-8 border-b-2 border-gray-200 text-left">
                     {session.title}
                   </td>
-                  <td className="py-2 px-4 border">{session.module_score}</td>
-                  <td className="py-2 px-4 border">{session.quiz_score}</td>
-                  <td className="py-2 px-4 border">
-                    {session.assignment_score}
-                  </td>
-                  <td className="py-2 px-4 border">{session.average_score}</td>
-                  <td className="py-2 px-4 border">
-                    {session.average_score_letter}
-                  </td>
+                  <td className="p-8 border">{session.module_score}</td>
+                  <td className="p-8 border">{session.quiz_score}</td>
+                  <td className="p-8 border">{session.assignment_score}</td>
+                  <td className="p-8 border">{session.average_score}</td>
+                  <td className="p-8 border">{session.average_score_letter}</td>
                 </tr>
               ))}
             </tbody>
           </table>
         </div>
+        <Link
+          to={`/score`}
+          className="flex items-center gap-2 p-5 pt-8 underline justify-start text-blue-500"
+        >
+          <FaArrowLeft />
+          Kembali
+        </Link>
       </div>
     </div>
   );
