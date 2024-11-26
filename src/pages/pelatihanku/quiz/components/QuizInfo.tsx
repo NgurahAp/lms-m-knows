ca69769824829f049b4139539108409903dadb6b
Arya <ngurahpratama2002@gmail.com>
2024-11-26 15:00:03 +0700

    Done Responsive quiz

--- src/pages/pelatihanku/quiz/components/QuizInfo.tsx
+++ src/pages/pelatihanku/quiz/components/QuizInfo.tsx
@@ -1,4 +1,4 @@
-import { DetailQuizResponse} from "../../../../types/pelatihanku/quiz";
+import { DetailQuizResponse } from "../../../../types/pelatihanku/quiz";
 import { GoListOrdered } from "react-icons/go";
 import { MdAccessAlarm } from "react-icons/md";
 import { PiExam } from "react-icons/pi";
@@ -15,21 +15,24 @@ export const QuizInfo = ({ quizData }: QuizInfoProps) => {
   return (
     <div>
       <div>
-        <h1 className="text-xl font-semibold pt-5 pb-2">Detail Quiz</h1>
-        <div className="flex items-center gap-x-2 py-2">
-          <GoListOrdered className="text-lg text-blue-500" />{" "}
+        <h1 className="text-base md:text-xl font-semibold pt-5 pb-2">
+          Detail Quiz
+        </h1>
+        <div className="flex items-center md:text-base text-sm gap-x-2 py-2">
+          <GoListOrdered className="text-base md:text-lg text-blue-500" />{" "}
           {quizData?.data.quiz.total_questions} Soal
         </div>
-        <div className="flex items-center gap-x-2 py-2">
-          <MdAccessAlarm className="text-lg text-blue-500" /> Durasi{" "}
+        <div className="flex items-center md:text-base text-sm gap-x-2 py-2">
+          <MdAccessAlarm className="text-base md:text-lg text-blue-500" /> Durasi{" "}
           {durationInMinutes} Menit
         </div>
-        <div className="flex items-center gap-x-2 py-2">
-          <PiExam className="text-lg text-blue-500" /> Nilai Kelulusan minimal
-          80
+        <div className="flex items-center md:text-base text-sm gap-x-2 py-2">
+          <PiExam className="text-base md:text-lg text-blue-500" /> Nilai
+          Kelulusan minimal 80
         </div>
-        <div className="flex items-center gap-x-2 py-2">
-          <FaHistory className="text-lg text-blue-500" /> Max. 3x Pengulangan
+        <div className="flex items-center md:text-base text-sm gap-x-2 py-2">
+          <FaHistory className="text-base md:text-lg text-blue-500" /> Max. 3x
+          Pengulangan
         </div>
       </div>
     </div>
