2f876850dcb5392eced0ec869fc1e543c464fa4f
Arya <ngurahpratama2002@gmail.com>
2024-11-26 13:17:35 +0700

    Done Modul, dashboard, pelatihan responsive

--- src/pages/pelatihanku/module/DetailModule.tsx
+++ src/pages/pelatihanku/module/DetailModule.tsx
@@ -129,7 +129,7 @@ export const DetailModule = () => {
             <div className="flex justify-center">
               <div className="flex justify-center">
                 {data?.module.status === "FINISHED" ? (
-                  <button className="mt-4 px-20 py-4 flex rounded-lg items-center bg-green-500 text-base md:text-xl gap-3 text-white hover:bg-green-600">
+                  <button className="mt-4 md:px-20 px-10 py-4 flex rounded-lg items-center bg-green-500 text-base md:text-xl gap-3 text-white hover:bg-green-600">
                     <div className="bg-white rounded-full">
                       <FaCheck className="text-green-600 p-1 text-xl" />
                     </div>
