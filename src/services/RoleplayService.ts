59ebb223044b87692871f77b652eed696c81bc4a
zahrahatiqah <zahrahatiqah1207@gmail.com>
2024-11-26 14:08:22 +0700

    perbaikan eror (1)

--- src/services/RoleplayService.ts
+++ src/services/RoleplayService.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
-import { RoleplayResponse } from "../types/roleplay";
+import { RoleplayData } from "../types/roleplay";
 import Cookies from "js-cookie";
 import { API_BASE_URL } from "../config/api";
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
 
-export const fetchRoleplayData = async (): Promise<RoleplayResponse> => {
+export const fetchRoleplayData = async (): Promise<RoleplayData> => {
   const token = Cookies.get("accessToken");
   const response = await axios.get(`${API_BASE_URL}/api/v1/student/roleplays`, {
     headers: {
@@ -11,4 +12,11 @@ export const fetchRoleplayData = async (): Promise<RoleplayResponse> => {
     },
   });
   return response.data;
+};
+
+export const useRoleplayData = (): UseQueryResult<RoleplayData, Error> => {
+  return useQuery<RoleplayData, Error>({
+    queryKey: ["roleplayData"],
+    queryFn: fetchRoleplayData,
+  });
 };
